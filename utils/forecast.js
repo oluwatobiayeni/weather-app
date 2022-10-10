80deb65bd60647482387532a5e0229df8fb5a6ba
oluwatobiayeni <oluwatobiayeni6@gmail.com>
2022-10-10 18:21:22 +0100

    test

---
+++ utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+const forecast= (latitude,longitude,callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=0532abc710196d7af96645d48c8cd429&query='+latitude +','+longitude+' &units=f'
+    request({url,json:true }, (error,{body})=>{
+        if (error) {
+            callback('unable to connect to weather services')
+        }
+        else if (response.body.error){
+            callback('unable to find location')
+        }
+        else {
+            const temp= body.current.temperature
+            const like= body.current.feelslike
+            callback(undefined,'it is currently'+temp,'degrees out')
+            callback(undefined,'It feels like',+like,'degrees out.')
+        }
+    })
+} 
+module.exports= forecast
